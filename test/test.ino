38e126cdc758330e2803c117420ed39db5f11fba
Bailonis <p.bailao@campus.fct.unl.pt>
2023-10-22 13:51:21 +0100

    del: retrieve yaw calculation

--- test/test.ino
+++ test/test.ino
@@ -1,24 +1,24 @@
 #include "ICM20600.h"
 #include "AK09918.h"
 #include <Wire.h>
-#include <math.h>
 
 ICM20600 icm20600(true);
 AK09918 ak09918;
 
-const int FLEX_SENSOR_PIN = A0; // Replace with the appropriate analog pin connected to the flex sensor
+const int FLEX_SENSOR_PIN = A0;
 const float ALPHA = 0.98;
 const float GYRO_CONVERSION_FACTOR = 131.0;
-const int DELAY_TIME = 1000; // Reduced delay to 1 second for better data logging
+const int DELAY_TIME = 1000;
 
-double roll, pitch, yaw;
+double roll, pitch;
 
-double baselineRoll, baselinePitch, baselineFlex; // Baseline values
-double rollThreshold = 10.0;  // Define your thresholds
+double baselineRoll, baselinePitch, baselineFlex;
+double rollThreshold = 10.0;
 double pitchThreshold = 10.0;
-int flexThreshold = 50; // Arbitrary value, adjust as needed
+int flexThreshold = 50;
 
-bool isCalibrating = true; // Calibration flag
+bool isCalibrating = true;
+unsigned long lastUpdateTime = 0; // For non-blocking delay
 
 double calculateRoll(int16_t acc_x, int16_t acc_y, int16_t acc_z) {
     return atan2((float)acc_y, (float)acc_z) * 180.0 / M_PI;
@@ -34,38 +34,39 @@ void setup() {
     icm20600.initialize();
     ak09918.initialize();
     Serial.begin(9600);
-    yaw = 0;
+  
 
     Serial.println("Starting Calibration... Maintain a neutral posture!");
-    delay(5000); // Give 5 seconds for user to assume a neutral posture
-    calibrateSensors(); // Establish baseline
+    delay(5000);
+    calibrateSensors();
     Serial.println("Calibration Complete!");
     isCalibrating = false;
 }
 
 void calibrateSensors() {
-    // Fetch and set baseline values
     baselineRoll = calculateRoll(icm20600.getAccelerationX(), icm20600.getAccelerationY(), icm20600.getAccelerationZ());
     baselinePitch = calculatePitch(icm20600.getAccelerationX(), icm20600.getAccelerationY(), icm20600.getAccelerationZ());
     baselineFlex = analogRead(FLEX_SENSOR_PIN);
+
 }
 
 void loop() {
-    // ... [Rest of the orientation code] ...
+    if (millis() - lastUpdateTime >= DELAY_TIME) {
+        lastUpdateTime = millis();
 
-    // Detect rounded shoulders based on deviations from baseline
-    if (!isCalibrating && (abs(roll - baselineRoll) > rollThreshold || abs(pitch - baselinePitch) > pitchThreshold || abs(analogRead(FLEX_SENSOR_PIN) - baselineFlex) > flexThreshold)) {
-        Serial.println("Rounded Shoulders Detected!");
+        roll = ALPHA * roll + (1.0 - ALPHA) * calculateRoll(icm20600.getAccelerationX(), icm20600.getAccelerationY(), icm20600.getAccelerationZ());
+        pitch = ALPHA * pitch + (1.0 - ALPHA) * calculatePitch(icm20600.getAccelerationX(), icm20600.getAccelerationY(), icm20600.getAccelerationZ());
+       
+
+        if (!isCalibrating && (abs(roll - baselineRoll) > rollThreshold || abs(pitch - baselinePitch) > pitchThreshold || abs(analogRead(FLEX_SENSOR_PIN) - baselineFlex) > flexThreshold)) {
+            Serial.println("Rounded Shoulders Detected!");
+        }
+
+        logData();
     }
-
-    // Data logging (assuming SD card module or other storage means)
-    logData();
-
-    delay(DELAY_TIME);
 }
 
 void logData() {
-    // Store data in a preferred manner. Here, we're just printing to the serial monitor
     Serial.print("Roll: "); Serial.print(roll);
     Serial.print("\tPitch: "); Serial.print(pitch);
     Serial.print("\tFlex Value: "); Serial.println(analogRead(FLEX_SENSOR_PIN));
