9c9034d652963902f64b8eb87c359feef157ece0
Bailonis <p.bailao@campus.fct.unl.pt>
2023-10-29 12:15:10 +0000

    ESP32, sends and receives data correctly

--- bluetoothconnectionTest/bluetoothconnectionTest.ino
+++ bluetoothconnectionTest/bluetoothconnectionTest.ino
@@ -1,25 +1,39 @@
 #include "BluetoothSerial.h"
-#include <WiFi.h>
 
 BluetoothSerial SerialBT;
+
+const int ledPin = 2;
+int counter = 0;
 
 void setup() {
   Serial.begin(9600);
   SerialBT.begin("ESP32"); // Name of your ESP32 Bluetooth device
+  
+  pinMode(ledPin, OUTPUT);  // Set ledPin as OUTPUT
+  digitalWrite(ledPin, LOW);  // Initially set it to LOW (LED OFF)
+  
   delay(500);
-   // Get the MAC address of the ESP32
-  String mac = WiFi.macAddress();
-  Serial.println("MAC address of this ESP32: " + mac);
+
   Serial.println("Bluetooth device is ready to pair");
- 
 }
 
 void loop() {
+  // Check if a Bluetooth client is connected
+  if (SerialBT.hasClient()) {
+    digitalWrite(ledPin, HIGH);  // Turn on the LED
+    if(counter == 0){
+      Serial.println("Bluetooth device paired");
+      SerialBT.print("14\n");  // Append a newline character 
+      counter++;
+    }
+   
+
+  } else {
+    digitalWrite(ledPin, LOW);  // Turn off the LED
+  }
+  
   if (SerialBT.available()) {
     char readChar = SerialBT.read();
-    SerialBT.write(readChar); // Echo whatever is received
     Serial.write(readChar);   // Also print to serial for debugging
   }
 }
-
-
